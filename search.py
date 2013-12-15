697e7fe54eca69c527bf8aa2e769bcc5aaf64462
Maxim Chernyatevich <vechnoe.info@gmail.com>
2013-12-15 21:18:37 +0000

    Изменил сохранение результатов поиска в список, из-за перезаписи ключей словаря.

--- search.py
+++ search.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Поиск имен святых.
+Поиск имен святых в календаре.
 
 """
 
@@ -23,37 +23,21 @@ month_word = {
     12: 'декабря'
 }
 
-search_string = 'Николы'
+search_string = 'Рожество'
 d = menology
-dict_result = {}
+list_result = []
 
 #Ищем в menology строку; если есть, добавляем в словарь.
 for key, value in d.iteritems():
     for key1, value1 in d[key].iteritems():
         for key2, value2 in d[key][key1].iteritems():
             if search_string in str(value2):
-                dict_result.setdefault(key, {}).update({key1: value2})
+                list_result.extend([[key, [key1, value2]]])
 
-#Меням key на строки с названиями месяцев.
-out = {}
-for key, value in month_word.iteritems():
-    try:
-        out.setdefault(value, dict_result[key])
-    except KeyError:
-        continue
+#Меняем 0 элемент вложенного списка на значения словаря с названиями месяцев.
+out = map(lambda s: s[0] in month_word and [month_word[s[0]], s[1]] or s, list_result)
 
 #Вывод результата.
-for key, value in out.iteritems():
-    for key1, value2 in out[key].iteritems():
-        print key1, key, value2
-
-
-
-
-
-
-
-
-
-
+for index, item in out:
+    print item[0], index, 'по ст. ст.', item[1]
 
